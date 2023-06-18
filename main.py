9eadbeb6a28e81a8356e1171a92957ffcd0dd736
VadimZakharikov <vadim.zakharikov@gmail.com>
2023-06-18 23:51:51 +0300

    Add base bot logic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-from os.path import join, dirname
 import telebot
 import logging
 from config import *
@@ -30,4 +29,3 @@ if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
