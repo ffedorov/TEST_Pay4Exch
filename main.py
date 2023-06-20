441cf027b64538a737a5fbca1905eb4e493c8324
VadimZakharikov <vadim.zakharikov@gmail.com>
2023-06-20 08:16:57 +0300

    Add database connection

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def start(message):
     db_oject.execute(f"SELECT id FROM users WHERE id = {id}")
     result = db_oject.fetchone()
 
-    if not result:
+#    if not result:
     db_oject.execute("INSERT INTO users(id, username, usercontact) VALUES (%s, %s, %s)", (id, username, ''))
     db_connection.commit()
 #    bot.reply_to(message, f"Hello, {username}!")
